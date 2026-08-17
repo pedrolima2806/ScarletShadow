be8c86aa5078d6d8877dc667759d0318ab73471e
pedrolima2806 <pedrolimasilva2003@gmail.com>
2026-08-17 10:43:01 -0300

    estrutura do main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,100 @@
+#include <chrono>
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    //=============
+    //INICIALIZAÇÃO
+    //=============
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return 1;
     }
+    if (!TTF_Init()) {
+        std::cerr << "TTF_Init failed." << std::endl;
+    }
+    //=================
+    //WINDOW E RENDERER
+    //=================
+    int width = 640, height = 480;
+    SDL_Window *window = SDL_CreateWindow("hellcat_mp", width, height, 0);
+    if (!window) {
+        std::cerr << "Failed to create window:" << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        std::cerr << "Failed to create renderer:" << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    //=====
+    //FONTE
+    //=====
+    TTF_Font *font = TTF_OpenFont("../assets/fonts/Prata/Prata-Regular.ttf", 16);
+    if (!font) {
+        std::cerr << "Failed to open font:" << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    //==========
+    //LOOP CYCLE
+    //==========
+    auto previousTime = std::chrono::steady_clock::now();
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT ||
+                event.type == SDL_EVENT_KEY_DOWN && event.key.scancode == SDL_SCANCODE_ESCAPE) {
+                running = false;
+                }
+            //====================
+            //HANDLE EVENT SECTION
+            //====================
+            //musicPlayer.handleEvent(event);
+        }
+        //BACKGROUND
+        SDL_SetRenderDrawColor(renderer, 20, 20, 30, 255);
+        SDL_RenderClear(renderer);
+
+        //TEXT RENDERING
+
+        //DELTA MS
+
+        //==============
+        //UPDATE SECTION
+        //==============
+        //musicPlayer.update(deltaMs);
+
+        //=================
+        //RENDERING SECTION
+        //=================
+        //musicPlayer.render(renderer, musicPlayerTexture);
+
+        //===========================
+        //ANIMATION RENDERING SECTION
+        //===========================
+
+        SDL_RenderPresent(renderer);
+    }
+    //============
+    //FINALIZATION
+    //============
+
+    //MIX_DestroyMixer(mixer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_Quit();
+    //MIX_Quit();
+    SDL_Quit();
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
